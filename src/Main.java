f4fd8fab48b878554ce3d9bb847cd41ebbb0c30a
Mariia Nazarova <Djungary@mail.ru>
2022-12-16 14:01:49 +0300

    change

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double amount = 1_000_000; // сумма кредита
-        double percent = 9.99; // проценты
-        double month = 12; // срок взятия
-        int payment = (int) service.canculate(amount, percent, month); // аннуитетный платёж
-
-        //System.out.println(payment);
 
         System.out.println((int) service.canculate(1_000_000, 9.99, 12));
         System.out.println((int) service.canculate(1_000_000, 9.99, 24));
