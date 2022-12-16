69ecb75875486cd523dfffb366b33c0c6741892c
Mariia Nazarova <Djungary@mail.ru>
2022-12-16 13:13:21 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
         double amount = 1_000_000; // сумма кредита
-        double percent = 9.99/100; // проценты, переведенные в число для расчетов
+        double percent = 9.99 / 100; // проценты, переведенные в число для расчетов
         double month = 12; // срок взятия
         int payment = (int) service.canculate(amount, percent, month); // аннуитетный платёж
 
-        System.out.println( (int) service.canculate(1_000_000, 9.99/100, 12));
-        System.out.println( (int) service.canculate(1_000_000, 9.99/100, 24));
-        System.out.println( (int) service.canculate(1_000_000, 9.99/100, 36));
+        System.out.println((int) service.canculate(1_000_000, 9.99 / 100, 12));
+        System.out.println((int) service.canculate(1_000_000, 9.99 / 100, 24));
+        System.out.println((int) service.canculate(1_000_000, 9.99 / 100, 36));
     }
 }
