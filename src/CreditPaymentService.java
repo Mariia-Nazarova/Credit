dbe585bdb0fbcbe6cb89d9e33207dcb05eacddca
Mariia Nazarova <Djungary@mail.ru>
2022-12-16 13:38:51 +0300

    change

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
     public double canculate(double amount, double percent, double month) {
-        double i = ((percent * (month / 12)) / month);
+        double i = (percent/100) * ((month / 12) / month);
         double k = i + i / (Math.pow(1 + i, month) - 1);
         double a = k * amount;
 
